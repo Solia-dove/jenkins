6280163922812a717a465c5aa371bcfcbffca491
Solia-dove <solime27@gmail.com>
2022-02-13 18:05:27 -0600

    first commit

---
+++ src/test/java/com/example/jenkinsspringboot/JenkinsSpringbootApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.jenkinsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class JenkinsSpringbootApplicationTests {
+    public static Logger logger= LoggerFactory.getLogger((JenkinsSpringbootApplicationTests.class));
+
+    @Test
+    void contextLoads() {
+        assertEquals(true,true);
+    }
+
+}
