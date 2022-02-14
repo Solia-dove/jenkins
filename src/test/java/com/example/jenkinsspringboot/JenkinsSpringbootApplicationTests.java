4820e99c090a499c84b7725008437caef68d62ca
Solia-dove <solime27@gmail.com>
2022-02-13 19:02:24 -0600

    second commit

--- src/test/java/com/example/jenkinsspringboot/JenkinsSpringbootApplicationTests.java
+++ src/test/java/com/example/jenkinsspringboot/JenkinsSpringbootApplicationTests.java
@@ -16,6 +16,9 @@ class JenkinsSpringbootApplicationTests {
 
     @Test
     void contextLoads() {
+        logger.info("Test case executing.....");
+        logger.info("Test case executing second log statement.....");
+
         assertEquals(true,true);
     }
 
